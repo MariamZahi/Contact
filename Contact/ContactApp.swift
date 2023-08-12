a1d3c0f0a43f6e22282abc51c0d10030633f7446
Mariam Zahid <mzahidmalhi@hotmail.com>
2023-08-11 22:00:13 -0400

    Initial Commit

---
+++ Contact/ContactApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactApp.swift
+//  Contact
+//
+//  Created by Waqar on 2023-08-11.
+//
+
+import SwiftUI
+
+@main
+struct ContactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
